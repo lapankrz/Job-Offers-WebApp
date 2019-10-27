5a3e51efa1b96b9180927b938eafa44156601c3e
Krzysztof Łapan <01130673@pw.edu.pl>
2019-10-27 15:24:11 +0100

    basic apply layout

--- WebApp/Models/JobOffer.cs
+++ WebApp/Models/JobOffer.cs
@@ -9,5 +9,11 @@ namespace WebApp.Models
     {
         public int Id { get; set; }
         public string JobTitle { get; set; }
+        public string JobDescription { get; set; }
+        public Company Company { get; set; }
+        public string Location { get; set; }
+        public int Salary { get; set; }
+        public DateTime CreationDate { get; set; }
+        public List<JobApplication> JobApplications { get; set; } = new List<JobApplication>();
     }
 }
--- WebApp/Startup.cs
+++ WebApp/Startup.cs
@@ -7,8 +7,10 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using WebApp.EntityFramework;
 
 namespace WebApp
 {
@@ -24,6 +26,9 @@ namespace WebApp
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            var connection = Configuration["DatabaseConnectionString"];
+            services.AddDbContext<DataContext>(options => options.UseSqlServer(connection));
+
             services.Configure<CookiePolicyOptions>(options =>
             {
                 // This lambda determines whether user consent for non-essential cookies is needed for a given request.
@@ -58,6 +63,8 @@ namespace WebApp
                     name: "default",
                     template: "{controller=Home}/{action=Index}/{id?}");
             });
+
+            
         }
     }
 }
