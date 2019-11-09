925cabfcc231199c5a72e3e74a865d4b297bdf9b
Krzysztof Łapan <01130673@pw.edu.pl>
2019-11-09 13:31:23 +0100

    Validation added

--- WebApp/Controllers/JobOfferController.cs
+++ WebApp/Controllers/JobOfferController.cs
@@ -17,25 +17,45 @@ namespace WebApp.Controllers
         {
             context = dataContext;
         }
-        /*
-        [Route("index")]
-        public async Task<ActionResult> Create()
-        {
-            var offers = context.JobOffers.ToList();
-            return View(offers);
-        }
-        */
         [Route("index")]
         public IActionResult Index()
         {
             return View(context.JobOffers.ToList());
         }
-        
-
+        [Route("AddJobOffer")]
+        public IActionResult AddJobOffer()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult SaveJobOffer(JobOffer offer)
+        {
+            if (ModelState.IsValid)
+            {
+                var company = context.Companies.FirstOrDefault(c => c.Id == offer.Id);
+                offer.Company = company;
+                offer.Id = 0;
+                offer.CreationDate = DateTime.Now;
+                context.JobOffers.Add(offer);
+                context.SaveChanges();
+                return View("/Views/JobOffer/Index.cshtml", context.JobOffers.ToList());
+            }
+            return View("AddJobOffer");
+        }
+        [Route("Details")]
         public IActionResult Details(int id)
         {
             var offer = context.JobOffers.Include(o => o.Company).Include(o => o.JobApplications).FirstOrDefault(o => o.Id == id);
             return View(offer);
+        }
+        [Route("DeleteOffer")]
+        public IActionResult DeleteOffer(int id)
+        {
+            context.JobApplications.RemoveRange(context.JobApplications.Where(a => a.OfferId == id));
+            context.JobOffers.RemoveRange(context.JobOffers.Where(o => o.Id == id));
+            context.SaveChanges();
+            return RedirectToAction("Index");
         }
     }
 }
---
+++ WebApp/Models/DataAttributes.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApp.Models
+{
+    public class IsDateAfterAttribute : ValidationAttribute
+    {
+        private readonly string testedPropertyName;
+        public IsDateAfterAttribute(string testedPropertyName)
+        {
+            this.testedPropertyName = testedPropertyName;
+        }
+
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            //value = DateTime.Now;
+            var propertyTestedInfo = validationContext.ObjectType.GetProperty(this.testedPropertyName);
+            if (propertyTestedInfo == null)
+            {
+                return new ValidationResult(string.Format("unknown property {0}", this.testedPropertyName));
+            }
+
+            var propertyTestedValue = propertyTestedInfo.GetValue(validationContext.ObjectInstance, null);
+            propertyTestedValue = DateTime.Now;
+            if (value == null || !(value is DateTime))
+            {
+                return ValidationResult.Success;
+            }
+
+            if (propertyTestedValue == null || !(propertyTestedValue is DateTime))
+            {
+                return ValidationResult.Success;
+            }
+
+            // Compare values
+            if ((DateTime)value >= (DateTime)propertyTestedValue)
+            {
+                if (value == propertyTestedValue)
+                {
+                    return ValidationResult.Success;
+                }
+                else if ((DateTime)value > (DateTime)propertyTestedValue)
+                {
+                    return ValidationResult.Success;
+                }
+            }
+
+            return new ValidationResult(GetErrorMessage());
+        }
+
+        public string GetErrorMessage()
+        {
+            return "Valid until date cannot be in the past!";
+        }
+    }
+
+    public class SalaryToIsBiggerAttribute : ValidationAttribute
+    {
+        private readonly string testedPropertyName;
+        public SalaryToIsBiggerAttribute(string testedPropertyName)
+        {
+            this.testedPropertyName = testedPropertyName;
+        }
+
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            var propertyTestedInfo = validationContext.ObjectType.GetProperty(this.testedPropertyName);
+            if (propertyTestedInfo == null)
+            {
+                return new ValidationResult(string.Format("unknown property {0}", this.testedPropertyName));
+            }
+
+            var propertyTestedValue = propertyTestedInfo.GetValue(validationContext.ObjectInstance, null);
+            if (value == null || !(value is int))
+            {
+                return new ValidationResult("Input correct values");
+            }
+
+            if (propertyTestedValue == null || !(propertyTestedValue is int))
+            {
+                return new ValidationResult("Input correct values");
+            }
+
+            if ((int)propertyTestedValue <= 0 || (int)value <= 0)
+            {
+                return new ValidationResult("Salary values must be greater than 0!");
+            }
+
+            // Compare values
+            if ((int)value >= (int)propertyTestedValue)
+            {
+                if (value == propertyTestedValue)
+                {
+                    return ValidationResult.Success;
+                }
+                else if ((int)value > (int)propertyTestedValue)
+                {
+                    return ValidationResult.Success;
+                }
+            }
+
+            return new ValidationResult(GetErrorMessage());
+        }
+
+        public string GetErrorMessage()
+        {
+            return "Salary to must be greater than salary from!";
+        }
+    }
+}
