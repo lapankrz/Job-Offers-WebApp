78c73e9af5e79ddf5a79230abbd226fcff6f196c
Krzysztof Łapan <01130673@pw.edu.pl>
2020-01-01 19:17:25 +0100

    readded Models folder (accidentally deleted earlier)

---
+++ WebApp/Models/JobOffer.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApp.Models
+{
+    public class JobOffer
+    {
+        public int Id { get; set; }
+        [Required]
+        public string JobTitle { get; set; }
+        public string JobDescription { get; set; }
+        [Required]
+        public Company Company { get; set; }
+        [Required]
+        public string Location { get; set; }
+        [Required]
+        public int SalaryFrom { get; set; }
+        [Required]
+        [SalaryToIsBigger("SalaryFrom")]
+        public int SalaryTo { get; set; }
+        public DateTime CreationDate { get; set; }
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        [IsDateAfter("CreationDate")]
+        [Required]
+        public DateTime ValidUntil { get; set; }
+        public List<JobApplication> JobApplications { get; set; } = new List<JobApplication>();
+    }
+}
